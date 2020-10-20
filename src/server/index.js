a80a5e7f030f3cc33da16bcd416b262ab9f1950f
ThiryS <thiry.simon1@gmail.com>
2020-10-20 15:29:58 +0200

    addLeaves and modif user

--- src/server/index.js
+++ src/server/index.js
@@ -50,7 +50,6 @@ MongoClient.connect(url, (err, client) => {
     app.post("/buytree", trees.buyOneTree);
     app.get("/logs", logs.getAllLogs);
 
-
     app.listen(APP_PORT, () =>
         console.log(`🚀 Server is listening on port ${APP_PORT}.`),
     );
