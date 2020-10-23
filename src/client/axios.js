e5564b58ba2916e55139ce88bfbca104037a2608
donovanherion <donovanherion@gmail.com>
2020-10-23 12:48:37 +0200

    changed base url react

--- src/client/axios.js
+++ src/client/axios.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:80/",
+    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:80/",
 });
 
 export default instance;
